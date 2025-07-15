0f31b186e2ea2685322cfe536801d8fa02b1f5f0
matcir <matteocirilloboolean@gmail.com>
2025-07-15 15:01:03 +0200

    refactor AppBrowse component to use a dynamic items array for rendering

--- src/components/AppBrowse.jsx
+++ src/components/AppBrowse.jsx
@@ -1,28 +1,48 @@
 export default function() {
+    
+    const items = [
+        {
+            classNameImage: "icon",
+            src: "/img/buy-comics-digital-comics.png",
+            classNameLink: "link",
+            text: "DIGITAL COMICS"
+        },
+        {
+            classNameImage: "icon",
+            src: "/img/buy-comics-merchandise.png",
+            classNameLink: "link",
+            text: "DC MERCHANDISE"
+        },
+        {
+            classNameImage: "icon",
+            src: "/img/buy-comics-subscriptions.png",
+            classNameLink: "link",
+            text: "DIGITAL COMICS"
+        },
+        {
+            classNameImage: "icon",
+            src: "/img/buy-comics-shop-locator.png",
+            classNameLink: "link",
+            text: "COMIC SHOP LOCATOR"
+        },
+        {
+            classNameImage: "icon",
+            src: "/img/buy-dc-power-visa.svg",
+            classNameLink: "link",
+            text: "DC POWER VISA"
+        },
+    ]
+    
     return(
         <>
         <div className="bg-primary browse-area">
             <div className="container d-flex justify-content-around pt-5">
-                <div className="item">
-                    <img className="icon" src="/img/buy-comics-digital-comics.png" />
-                    <a className="link" href="">DIGITAL COMICS</a>
-                </div>
-                <div className="item">
-                    <img className="icon" src="/img/buy-comics-merchandise.png" />
-                    <a className="link" href="">DC MERCHANDISE</a>
-                </div>
-                <div className="item">
-                    <img className="icon" src="/img/buy-comics-subscriptions.png" />
-                    <a className="link" href="">SUBSCRIPTION</a>
-                </div>
-                <div className="item">
-                    <img className="icon" src="/img/buy-comics-shop-locator.png" />
-                    <a className="link" href="">COMIC SHOP LOCATOR</a>
-                </div>
-                <div className="item">
-                    <img className="icon" src="/img/buy-dc-power-visa.svg" />
-                    <a className="link" href="">DC POWER VISA</a>
-                </div>
+                {items.map((item, id) => (
+                    <div className="item" key={id}>
+                        <img className={item.classNameImage} src={item.src} alt={item.text} />
+                        <a className={item.classNameLink} href="#">{item.text}</a>
+                    </div>
+                ))}
             </div>
         </div>
         </>
